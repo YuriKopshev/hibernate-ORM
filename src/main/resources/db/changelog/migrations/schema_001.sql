d219573445d6caa852cfcdeff1f387074bc58adb
Yuri Kopshev <yura.kopshev@gmail.com>
2022-10-02 13:32:26 +0200

    added liquibase migration

---
+++ src/main/resources/db/changelog/migrations/schema_001.sql
@@ -0,0 +1,2 @@
+
+INSERT INTO persons (age, name, surname, city_of_living, phone_number) VALUES (38,'Yuri','Petrov','Moscow','23445566');
---
+++ src/main/resources/db/changelog/migrations/schema_002.sql
@@ -0,0 +1,1 @@
+INSERT INTO persons (age, name, surname, city_of_living, phone_number) VALUES (25,'Ivan','Kozlov','Moscow','232323');
